57b4529f98f495573d880a9e0448727e922a9978
Victoria Dickey <vdickey@mlml.calstate.edu>
2019-05-14 18:47:09 -0700

    changed effect size and the description of effect size

--- analysis/Power_analysis.py
+++ analysis/Power_analysis.py
@@ -8,8 +8,8 @@ Created on Fri May  3 15:54:10 2019
 #We can use the statsmodels.stats power function solve_power to help us find the variable we are looking for
 #In this case, we are looking for the number of samples needed
 #Inputs are an effect size,nobs,significance level, and target power. 
-#Effect size can be thought of as the mean/standard deviation. Or, it can be thought of as the significant number 
-#that can cause a separation the data values/the distance between data values
+#Effect size for this project is the significant difference between particle diameters 
+#divided by the standard deviation of the means of the samples collected from the dunes, which are our control samples 
 #nobs is the number of samples needed
 #significance level is alpha = 0.05 for a significance level of 95%
 #target power is 1-beta, usually 0.8
@@ -19,5 +19,5 @@ from __future__ import print_function
 
 from statsmodels.stats import power as p
 
-samplenumber = p.FTestAnovaPower().solve_power(effect_size=0.33,nobs=None,alpha=0.05,power=0.8)
+samplenumber = p.FTestAnovaPower().solve_power(effect_size=2.6,nobs=None,alpha=0.05,power=0.8)
 print(samplenumber)
